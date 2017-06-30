16a12d6ddd6e8650f887b548d2f6b488ec6f9091
Javier Letelier <letelier.javier@gmail.com>
2017-06-30 02:09:10 -0300

    Webpack pug main

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const path = require('path');
+
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, '/app/views'));
+
+app.use(express.static('public'));
 
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
 app.get('/',function(req,res){
-	res.send('Hello word!');
+	res.render('express-index');
 });
 
 app.listen(3000,function(){
--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,36 @@
-var HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPugPlugin = require('html-webpack-pug-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const extractSass = new ExtractTextPlugin({
+  filename: "[name].[contenthash].css"
+});
+
 const path = require('path');
 module.exports = {
   entry: './app/webpack-index.js',
   output: {
-    path: path.resolve(__dirname, 'public/js'),
+    path: path.resolve(__dirname, 'public/'),
+    publicPath:'/',
     filename: 'webpack-index.bundle.js'
   },
   module: {
     rules: [{
       test: /\.scss$/,
-      use: [{
-        loader: "style-loader" // creates style nodes from JS strings
-      }, {
-        loader: "css-loader" // translates CSS into CommonJS
-      }, {
-        loader: "sass-loader" // compiles Sass to CSS
-      }]
-    },
-    {
-      test: /\.pug$/,
-      use: 'pug-loader'
+      use: extractSass.extract({
+        use: [{
+          loader: "css-loader"
+        }, {
+          loader: "sass-loader"
+        }],
+        // use style-loader in development
+        fallback: "style-loader"
+      })
     }]
   },
   plugins: [new HtmlWebpackPlugin({
-    template: 'app/views/index.pug'
-  })]
+    template: 'app/views/main_template.pug',
+    filename: '../app/views/main.pug',
+    filetype: 'pug',
+    inject:false
+  }),new HtmlWebpackPugPlugin(), extractSass]
 };
