a7d5fb082bb70048731a63469b3e8238fbdca3ab
Javier Letelier <letelier.javier@gmail.com>
2017-07-04 02:03:14 -0300

    Heroku port

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.get('/',function(req,res){
 	res.render('express-index');
 });
 
-app.listen(3000,function(){
-	console.log("Example app listening on port 3000");
+app.listen( process.env.PORT || 3000,function(){
+	console.log("Example app listening on port "+(process.env.PORT||3000));
 });
 
